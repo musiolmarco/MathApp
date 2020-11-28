5923fe01268d3d3cc8e82657afad857f6e90848a
Marco <0203marcom@gmail.com>
2020-11-28 17:35:54 +0100

    Changed "divider" symbol

--- lib/widgets/calculator/calculator_gridview.dart
+++ lib/widgets/calculator/calculator_gridview.dart
@@ -5,7 +5,7 @@ class CalculatorGridview extends StatelessWidget {
 
   final List _buttons = [
 
-    'C', '(', ')', '%',
+    'C', '(', ')', '÷',
     '7', '8', '9', '+',
     '4', '5', '6', '-',
     '1', '2', '3', 'x',
