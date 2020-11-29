1abdf3b1cb6f1dc0a2e831a62f3f7b8bd526f452
Marco <0203marcom@gmail.com>
2020-11-29 02:49:06 +0100

    GestureDetector to InkWell
    - changed GestureDetector to InkWell for tap effect
    - fixed Container
    - fixed tapping effect

--- lib/widgets/calculator/calculator_button.dart
+++ lib/widgets/calculator/calculator_button.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
 
 class CalculatorButton extends StatelessWidget {
-
   final Function pressed;
   final String displayValue;
   final Color color;
   final Color textColor;
 
-  CalculatorButton({@required this.pressed, @required this.displayValue, @required this.color, @required this.textColor});
+  CalculatorButton(
+      {@required this.pressed,
+      @required this.displayValue,
+      @required this.color,
+      @required this.textColor});
 
   @override
   Widget build(BuildContext context) {
-
     final width = MediaQuery.of(context).size.width;
     final heigth = MediaQuery.of(context).size.height;
 
+    return Container(
+      height: heigth * 0.105,
+      width: width * 0.2,
       decoration: BoxDecoration(
-    return GestureDetector(
-        onTap: () => pressed(),
-        child: Container(
-          height: heigth * 0.105,
-          width: width * 0.2,
-          decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.all(Radius.circular(20))),
+          color: color, borderRadius: BorderRadius.all(Radius.circular(20))),
+      child: Material(
+        child: InkWell(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          onTap: () => pressed(),
           child: Center(
             child: Text(
               displayValue,
@@ -34,6 +36,8 @@ class CalculatorButton extends StatelessWidget {
               ),
             ),
           ),
+        ),
+        color: Colors.transparent,
       ),
     );
   }
